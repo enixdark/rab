e1c17ca8f45b604f4d4ee4b90251c6fde3eb6895
enixdark <cqshinn92@gmail.com>
2015-03-28 20:34:56 +0700

    remove php files

--- php/php-amqplib/demo/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('PORT', 5672);
-define('USER', 'guest');
-define('PASS', 'guest');
-define('VHOST', '/');
-
-//If this is enabled you can see AMQP output on the CLI
-define('AMQP_DEBUG', true);
